9cc49a2383e23ea0c3c20322293280cedd36c655
MVitoriahub <vitoria.marchi@edu.unifil.br>
2025-05-20 20:57:33 -0300

    método contar nós

--- src/Arvore.java
+++ src/Arvore.java
@@ -1,3 +1,13 @@
 public class Arvore {
     No raiz;
+
+    public int contarNos(){
+        return  contarNosRecursivo(raiz);
+    }
+    private int contarNosRecursivo(No no){
+        if(no == null){
+            return 0;
+        }
+        return 1+ contarNosRecursivo(no.esquerda)+ contarNosRecursivo(no.direita);
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Arvore arvore = new Arvore();
+
+
+        arvore.raiz = new No("a");
+        arvore.raiz.esquerda = new No("b");
+        arvore.raiz.direita = new No("c");
+        arvore.raiz.esquerda.esquerda = new No("d");
+        arvore.raiz.esquerda.direita = new No("e");
+        arvore.raiz.direita.direita = new No("f");
+
+
+        int totalNos = arvore.contarNos();
+        System.out.println("Total de nós na árvore: " + totalNos);
+    }
+}
